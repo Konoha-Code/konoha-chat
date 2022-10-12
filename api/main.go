c19e1b62ef579a293efe7dc229af03919a21669d
Eduardo Uliana <edu02_rko@hotmail.com>
2022-10-11 21:19:27 -0300

    Adicionado criação da estrutura

--- api/main.go
+++ api/main.go
@@ -1,10 +1,24 @@
 package main
 
-import "konoha-chat/server"
+import (
+	"konoha/api-go/database"
+	"konoha/api-go/models"
+	"konoha/api-go/server"
+)
 
 func main() {
 	// Start database
 	database.StartDB()
+
+	// Create table Message
+	database.GetDatabase().AutoMigrate(&models.Message{})
+
+	// Create table User
+	database.GetDatabase().AutoMigrate(&models.User{})
+
+	// Create server
 	server := server.NewServer()
+
+	// Start server
 	server.Run()
 }
