c790c95a04c6a9b0bcb098da84d7951328d9eb24
Eduardo Uliana <edu02_rko@hotmail.com>
2022-10-11 21:16:11 -0300

    Adicionado rotas possíveis

--- api/server/routes/route.go
+++ api/server/routes/route.go
@@ -1,15 +1,19 @@
 package routes
 
 import (
-	"konoha-chat/controllers"
+	"sysmo/api-go/controllers"
 
 	"github.com/gin-gonic/gin"
 )
 
 func ConfigRoutes(route *gin.Engine) *gin.Engine {
-	main := route.Group("konoha-chat/api")
+	main := route.Group("konoha/chat")
 
-	main.GET("/ping", controllers.Ping)
+	main.GET("/ping", controllers.GetPing)
+	main.GET("/createUser", controllers.CreateUser)
+	main.GET("/getUser", controllers.GetUser)
+	main.GET("/sendMessage", controllers.SendMessage)
+	main.GET("/getMessages", controllers.GetMessages)
 
 	return route
 }
